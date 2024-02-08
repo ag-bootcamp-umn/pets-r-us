c833acfac6794f73de0ce80588a5b04ae34a7bf9
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-02-08 14:15:17 -0600

    created database models and schema

---
+++ models/Appointment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Appointment extends Model {}
+
+Appointment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATETIME,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    pet_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'pet',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'appointment',
+  }
+);
+
+module.exports = Appointment;
