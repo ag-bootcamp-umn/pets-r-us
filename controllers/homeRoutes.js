0d758ccb86c800078165f842e08e6c7d6a702cec
alejandro <alexgeorgegeer@gmail.com>
2024-02-09 13:47:57 -0600

    starting on profiles routing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,14 @@ router.get('/', (req, res) => {
   res.render('home')
 });
 
+router.get('/meet', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/signin');
+    return;
+  }
+  res.render('signup');
+});
+
 router.get('/signin', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
