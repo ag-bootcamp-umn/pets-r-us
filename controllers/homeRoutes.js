f32b941a7c72aa430ac034147989ca826d54f63d
ag-bootcamp-umn <153794243+ag-bootcamp-umn@users.noreply.github.com>
2024-02-09 14:11:58 -0600

    Routing (#15)
    * added controllers directory and index.js
    
    * added index.js to controllers/api
    
    * about to merge with main
    
    * Started on authjs
    
    * got server running
    
    * pulling in profiles merge
    
    * starting on profiles routing
    
    * completed basic routing
    
    ---------
    
    Co-authored-by: alejandro <alexgeorgegeer@gmail.com>

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,21 +5,20 @@ router.get('/', (req, res) => {
   res.render('home')
 });
 
-router.get('/signin', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-  res.render('signin');
+router.get('/meet', (req, res) => {
+  res.render('meet');
+});
+
+router.get('/pets', (req, res) => {
+  res.render('petprofiles')
+});
+
+router.get('/user', (req, res) => {
+  res.render('profile');
 });
 
 router.get('/signup',(req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
   res.render('signup');
 });
-
 
 module.exports = router;
