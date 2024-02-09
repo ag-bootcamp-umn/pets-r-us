0960054f9954555218a346f0fa085d107cf6c189
alejandro <alexgeorgegeer@gmail.com>
2024-02-09 12:31:08 -0600

    pulling in profiles merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
   res.render('home')
-})
+});
+
+router.get('/signin', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signin');
+});
+
+router.get('/signup',(req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
 
 module.exports = router;
