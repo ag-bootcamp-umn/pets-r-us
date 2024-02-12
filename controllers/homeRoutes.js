d04797a2188e63e3a9f04514142d60739d146caa
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-02-12 15:29:37 -0600

    pet profiles and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,32 +16,33 @@ router.get('/meet', (req, res) => {
 router.get('/pets', async (req, res) => {
   
   try {
-    // const loggedIn = (req.session.loggedIn)? req.session.loggedIn : false;
-    // let petData;
-    // // non-logged in user
-    // if (!loggedIn) {
-    // petData = await Pet.findAll();
-    // const pets = petData.map((pet) => pet.get({ plain: true }));
-    // }
-    // // Logged IN user
-    // else {
-    const userData = await User.findByPk(3, {attributes: {exclude: ['password']}});
+    const loggedIn = (req.session.loggedIn)? req.session.loggedIn : false;
+    let petData;
+
+    // non-logged in user
+    if (!loggedIn) {
+    petData = await Pet.findAll();
+    const pets = petData.map((pet) => pet.get({ plain: true }));
+    }
+
+    // Logged IN user
+    else {
+    const userData = await User.findByPk(req.session.userId, {attributes: {exclude: ['password']}});
     const userInfo = userData.get({ plain: true });
-    console.log(userInfo);
-    const petData = await Pet.findAll({
-      where: { [Op.or]:[
+    
+    petData = await Pet.findAll({
+      where: [
         {species:userInfo.species},
         {hypoallergenic:userInfo.hypoallergenic},
         {kids_status:userInfo.kids_status}
-      ]}}
-      );
-    // }
+      ]});
+    }
     const pets = petData.map((pet) => pet.get({ plain: true }));
-    //const pets_number = pets.length;
+    const pets_number = pets.length;
     res.render('petprofiles', { 
-      //pets_number,
+      pets_number,
       pets, 
-      //loggedIn
+      loggedIn
     });
   } catch (err) {
     res.status(500).json({error:err.message});
