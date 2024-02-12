df156dc34a8e415dcca541c36ff4debc4626f66d
ag-bootcamp-umn <153794243+ag-bootcamp-umn@users.noreply.github.com>
2024-02-12 09:28:45 -0600

    Booking (#23)
    * added form-data capture to meet, changed names to appointment
    
    * sorted out routing for appointments
    
    ---------
    
    Co-authored-by: alejandro <alexgeorgegeer@gmail.com>

---
+++ controllers/api/appointment.routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Appointment } = require('../../models');
+
+// Getting booking input
+router.post('/', async (req, res) => {
+  console.log(req.body);
+  try {
+    console.log(req.body);
+    res.json({
+      msg: 'You did it!'
+    })
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
