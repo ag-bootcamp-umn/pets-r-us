8ca7759de25778951c3512ed2c5f2f7239922240
thejackbitt <jack.bittner@outlook.com>
2024-02-13 12:24:26 -0600

    fixed timer on logout page

--- public/js/accountLeave.js
+++ public/js/accountLeave.js
@@ -2,16 +2,14 @@ let timer = 5;
 
 const intervalId = setInterval(() => {
     $('#timer').text(timer);
-    console.log(timer);
     timer -= 1;
     
     if (timer < 0) {
         clearInterval(intervalId);
         $('#timer').text(0);
+        document.location.replace('/');
     }
 }, 1000);
-
-intervalId();
 
 const logout = async () => {
     const response = await fetch('/api/users/logout', {
@@ -20,9 +18,7 @@ const logout = async () => {
     });
   
     if (response.ok) {
-        setTimeout(() => {
-            document.location.replace('/');
-            }, 5000);
+        console.log("You've been signed out successfully.")
     } else {
       alert('You are not signed in.');
     }
