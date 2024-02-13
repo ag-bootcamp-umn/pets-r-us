55684e6ad60926955926ff0dcbdfb0b0258f95af
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-02-12 19:25:23 -0600

    Pet profiles  (#27)
    * added module to pet profiles
    
    * added user search to pet profiles
    
    * added where clause to petData in petprofiles
    
    * added Op.or
    
    * bug in userInfo
    
    * pet profiles and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const dayjs = require('dayjs');
 const withAuth = require('../utils/auth');
+const {User, Pet} = require("../models")
 
 router.get('/', (req, res) => {
   res.render('home')
@@ -12,8 +13,40 @@ router.get('/meet', (req, res) => {
   res.render('appointment', {now});
 });
 
-router.get('/pets', (req, res) => {
-  res.render('petprofiles')
+router.get('/pets', async (req, res) => {
+  
+  try {
+    const loggedIn = (req.session.loggedIn)? req.session.loggedIn : false;
+    let petData;
+
+    // non-logged in user
+    if (!loggedIn) {
+    petData = await Pet.findAll();
+    const pets = petData.map((pet) => pet.get({ plain: true }));
+    }
+
+    // Logged IN user
+    else {
+    const userData = await User.findByPk(req.session.userId, {attributes: {exclude: ['password']}});
+    const userInfo = userData.get({ plain: true });
+    
+    petData = await Pet.findAll({
+      where: [
+        {species:userInfo.species},
+        {hypoallergenic:userInfo.hypoallergenic},
+        {kids_status:userInfo.kids_status}
+      ]});
+    }
+    const pets = petData.map((pet) => pet.get({ plain: true }));
+    const pets_number = pets.length;
+    res.render('petprofiles', { 
+      pets_number,
+      pets, 
+      loggedIn
+    });
+  } catch (err) {
+    res.status(500).json({error:err.message});
+  }
 });
 
 router.get('/user', (req, res) => {
