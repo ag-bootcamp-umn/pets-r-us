c7a34bd035f191bfcdd9842816428854b2c15712
alejandro <alexgeorgegeer@gmail.com>
2024-02-16 14:40:35 -0600

    syncing with restoration

--- models/Pet.js
+++ models/Pet.js
@@ -26,17 +26,14 @@ Pet.init(
     species: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue:1
     },
     hypoallergenic: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      defaultValue:false
     },
     kids_status: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      defaultValue:true
     },
     dob: {
       type: DataTypes.DATE,
